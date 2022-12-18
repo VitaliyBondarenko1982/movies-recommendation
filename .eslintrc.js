e111f4786d7e1ec0449e19a3ca62bd7c520bed7e
Vitalii <vitalii.bondarenko.ua@gmail.com>
2022-12-18 21:03:41 +0200

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,148 @@
+module.exports = {
+  extends: ['standard', 'react-app', 'airbnb'],
+  env: {
+    es6: true,
+    browser: true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  overrides: [
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
+  rules: {
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        comments: 80,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    semi: ['error', 'always'],
+    'semi-style': ['error', 'last'],
+    'space-before-function-paren': ['error', 'never'],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    curly: ['error', 'all'],
+    'no-redeclare': ['error', { builtinGlobals: true }],
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: false,
+        allowTernary: false,
+        allowTaggedTemplates: false,
+      },
+    ],
+    'operator-linebreak': ['error', 'before'],
+    'no-console': 'error',
+    'standard/no-callback-literal': 0,
+    'brace-style': ['error', '1tbs'],
+    'object-curly-newline': [
+      2,
+      {
+        ObjectExpression: {
+          consistent: true,
+          minProperties: 4,
+        },
+      },
+    ],
+    'function-paren-newline': ['error', 'consistent'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: 1,
+        outerIIFEBody: 1,
+        FunctionDeclaration: {
+          parameters: 1,
+          body: 1,
+        },
+        FunctionExpression: {
+          parameters: 1,
+          body: 1,
+        },
+        CallExpression: {
+          arguments: 1,
+        },
+        ArrayExpression: 1,
+        ObjectExpression: 1,
+        ImportDeclaration: 1,
+        flatTernaryExpressions: false,
+        // list derived from
+        // https://github.com/benjamn/ast-types/blob/HEAD/def/jsx.js
+        // eslint-disable-next-line max-len
+        ignoredNodes: [
+          'JSXElement',
+          'JSXElement > *',
+          'JSXAttribute',
+          'JSXIdentifier',
+          'JSXNamespacedName',
+          'JSXMemberExpression',
+          'JSXSpreadAttribute',
+          'JSXExpressionContainer',
+          'JSXOpeningElement',
+          'JSXClosingElement',
+          'JSXFragment',
+          'JSXOpeningFragment',
+          'JSXClosingFragment',
+          'JSXText',
+          'JSXEmptyExpression',
+          'JSXSpreadChild',
+        ],
+        ignoreComments: false,
+      },
+    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-param-reassign': [2, { props: true }],
+    'no-shadow': ['error', { builtinGlobals: false }],
+    'react/destructuring-assignment': 0,
+    'jsx-a11y/label-has-for': [
+      2,
+      {
+        components: ['Label'],
+        required: {
+          some: ['id', 'nesting'],
+        },
+        allowChildren: true,
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [2, { assert: 'either' }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+      { blankLine: 'always', prev: 'directive', next: '*' },
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+    ],
+    'import/prefer-default-export': 0,
+  },
+};
